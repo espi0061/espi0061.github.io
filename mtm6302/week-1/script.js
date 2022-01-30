a16afd6c1f9b39848d2b9aa8a4458829dc0d5ac1
Santiago Espinosa Lizcano <espi0061@algonquinlive.com>
2022-01-30 15:38:05 -0500

    adding js first week

---
+++ mtm6302/week-1/script.js
@@ -0,0 +1,19 @@
+// string literal
+const firstName = 'Santiago'
+
+// number: Declaring and defining
+let age = 21
+
+// redefining 
+age = 30
+
+console.log('age', age)
+
+// Boolean
+const inClass = true
+
+
+// Template literal
+const message = `${firstName} is ${age} years old`
+//Santiago is 21 years old
+console.log(message)
